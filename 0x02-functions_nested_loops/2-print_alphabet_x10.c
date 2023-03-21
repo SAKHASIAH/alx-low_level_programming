beec8ce6f6c019b57a5c62f79ee3e4811d34275f
SAKHASIAH <sharonsakhasia8@gmail.com>
2023-03-21 08:46:23 -0700

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,7 +10,7 @@ void print_alphabet_x10(void)
 {
 	char c;
 	int i = 0;
-
+        
 	while (i <= 9)
 	{
 	for (c = 'a'; c <= 'z'; c++)
@@ -20,5 +20,4 @@ void print_alphabet_x10(void)
 	i++;
 	_putchar('\n');
 	}
-	
 }
